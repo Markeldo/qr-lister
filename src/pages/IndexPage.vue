215270df4b4424ca46dc4692499186b37daf3f6b
Markeldo <contact@marvic.ru>
2024-01-23 23:11:39 +0300

    Initial commit. FSD like template of Quasar framework

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <div class="myClass asdasdasd">привет</div>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+.myClass {
+  color: $primary
+}
+</style>
